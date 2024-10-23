0e076ca4ac6d78b9b1563dbc30b74a7937282689
shin <engus6001@naver.com>
2024-10-24 01:55:34 +0900

    userCheck

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -6,7 +6,8 @@ const taskController = {};
 taskController.createTask = async (req, res) => {
     try {
         const { task, isComplete } = req.body;
-        const newTask = new Task({ task, isComplete });
+        const { userId } = req;
+        const newTask = new Task({ task, isComplete, author: userId });
         await newTask.save();
         res.status(200).json({ status: "ok", data: newTask });
     } catch (err) {
@@ -17,7 +18,8 @@ taskController.createTask = async (req, res) => {
 // Select
 taskController.getTask = async (req, res) => {
     try {
-        const taskList = await Task.find({}).select("-__v");
+        // const taskList = await Task.find({}).select("-__v");
+        const taskList = await Task.find({}).populate("author"); // join과 같은
         res.status(200).json({ status: "ok", data: taskList });
     } catch (err) {
         res.status(400).json({ status: "fail", error: err });
